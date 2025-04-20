b464dc5c3d603177c421165e6acd9d2a47cd7cc6
Lara Pla <laraplamoreno@gmail.com>
2025-04-20 19:31:06 +0200

    docs: add index.html + script.js ++ feat: numbers exercises

---
+++ script.js
@@ -0,0 +1,126 @@
+// Ejercicios números
+
+function suma(num1, num2)
+{
+    console.log(num1 + num2)
+}
+suma (3, 4);
+
+function resta(num1, num2)
+{
+    console.log(num1 - num2)
+}
+resta (14, 4);
+
+function multipl(num1, num2)
+{
+    console.log(num1 * num2)
+}
+multipl (3, 4);
+
+function divi(num1, num2)
+{
+    console.log(num1 / num2)
+}
+divi (22, 5);
+
+function op5(base, exponente) 
+{
+    console.log(base ** exponente)
+}
+op5(7, 8);
+
+function op6(num1, num2)
+{
+    console.log(num1 % num2)   
+}
+op6(9, 2);
+
+function op7(num1)
+{
+    console.log(Math.sqrt(num1))   
+}
+op7(9);
+
+function op8(num1)
+{
+    console.log(Math.abs(num1))   
+}
+op8(-74);
+
+function op9(num1)
+{
+    console.log(Math.round(num1))   
+}
+op9(34.77);
+
+function op10()
+{
+    console.log(Math.random())   
+}
+op10();
+
+
+// Ejercicios letras
+
+suma("Hola", " caracola")
+
+function op11(text1)
+{
+    console.log(text1.length)   
+}
+op11("Mesas");
+
+function op12(text1)
+{
+    console.log(text1.toUpperCase())   
+}
+op12("teléfono");
+
+function op13(text1)
+{
+    console.log(text1.toLowerCase())   
+}
+op13("TELÉFONO");
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
+
+function op1()
+{
+    console.log()   
+}
+op1();
